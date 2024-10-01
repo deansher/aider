ae192d6146f3d38f1bafb8021006da0647a77c0f
deansher <deansherthompson@gmail.com>
2024-10-01 06:33:13 -0400

    Integrated Langfuse and took a first pass through improving prompts. Squashed commit of the following:
    commit 0d06d9aab4ccccacd486502764ab358f265a0936
    Author: deansher <deansherthompson@gmail.com>
    Date:   Tue Oct 1 06:32:16 2024 -0400
    
        improved prompts.py
    
    commit cbc89eb64c3c8eef6ef12cfbfe7b22db1afce666
    Author: deansher <deansherthompson@gmail.com>
    Date:   Tue Oct 1 06:00:24 2024 -0400
    
        improved prompts
    
    commit a21eb349b6a2db2863c58ae3d4ec89349adbd93b
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 13:43:21 2024 -0400
    
        improved prompts
    
    commit 5d3f78df3049a45ba548b3b673ff5b672c6b3fd3
    Author: deansher (aider) <deansherthompson@gmail.com>
    Date:   Mon Sep 30 13:17:54 2024 -0400
    
        fix: remove leading indentation from brade_persona_prompt to eliminate unintended whitespace
    
    commit d8221856a9db050c114b130f580c3032f08f8c48
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 13:17:53 2024 -0400
    
        feat: add Brade persona prompt for AI software engineer collaboration guidance
    
    commit 0ade0771e2b7e7c60a141f9d3c922342ee064cce
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 11:16:12 2024 -0400
    
        updated langfuse plan
    
    commit d8b904c8284e318f8b5382fd069091db0f7b22eb
    Author: deansher (aider) <deansherthompson@gmail.com>
    Date:   Mon Sep 30 11:02:26 2024 -0400
    
        fix: move Langfuse context configuration after environment variable setup in main.py
    
    commit 5e6f5024bb1b9b57a8be0ba4a21d872d8d8a366f
    Author: deansher (aider) <deansherthompson@gmail.com>
    Date:   Mon Sep 30 11:00:09 2024 -0400
    
        feat: add Langfuse context configuration after loading environment variables in main.py
    
    commit fb009331f8fb897a7f05c39cd3b54624a55ea325
    Author: deansher (aider) <deansherthompson@gmail.com>
    Date:   Mon Sep 30 10:59:07 2024 -0400
    
        feat: integrate Langfuse context configuration in main.py
    
    commit 1e1bdc944c9fa39181433e9f1f54e80ae1a1a153
    Author: deansher (aider) <deansherthompson@gmail.com>
    Date:   Mon Sep 30 10:54:42 2024 -0400
    
        chore: add Langfuse configuration section to sample.env file
    
    commit 83cd5e9bcc8ba4432763066debeb22df1f51d37d
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 10:49:18 2024 -0400
    
        tweaked langfuse plan
    
    commit 54b7db61dbbafb30b00f98c472d628ca2c596e71
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 10:47:45 2024 -0400
    
        add .env to .gitignore
    
    commit a82125ef085ba558ccc79e3261f81cd31bfb3f6f
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 10:47:34 2024 -0400
    
        update langfuse plan
    
    commit 307d894a5cc5a1b80e082991cbdf67a11ad6a2de
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 10:32:34 2024 -0400
    
        added langfuse to requirements
    
    commit 59d5a5c67bb1d02df55f625507a5f60bc07a2736
    Author: deansher <deansherthompson@gmail.com>
    Date:   Mon Sep 30 08:07:04 2024 -0400
    
        initial plan for Langfuse integration
    
    commit a1430751b659fe9c1380986d249917f9af74b140
    Author: deansher (aider) <deansherthompson@gmail.com>
    Date:   Mon Sep 30 07:43:02 2024 -0400
    
        feat: add langfuse_context import and configure Langfuse client in aider/llm.py
    
    commit cbe83b1d0f6e5a49dc72ca4578e205f8963a8cec
    Author: deansher (aider) <deansherthompson@gmail.com>
    Date:   Mon Sep 30 07:42:44 2024 -0400
    
        feat: integrate Langfuse tracing into send_completion function for LLM interactions

--- aider/main.py
+++ aider/main.py
@@ -11,6 +11,7 @@ import git
 import importlib_resources
 from dotenv import load_dotenv
 from prompt_toolkit.enums import EditingMode
+from langfuse.decorators import langfuse_context, observe
 
 from aider import __version__, models, urls, utils
 from aider.args import get_parser
@@ -429,6 +430,9 @@ def main(argv=None, input=None, output=None, force_git_root=None, return_coder=F
 
     # Load the .env file specified in the arguments
     loaded_dotenvs = load_dotenv_files(git_root, args.env_file, args.encoding)
+
+    # Configure Langfuse context using the loaded environment variables
+    langfuse_context.configure()
 
     # Parse again to include any arguments that might have been defined in .env
     args = parser.parse_args(argv)
