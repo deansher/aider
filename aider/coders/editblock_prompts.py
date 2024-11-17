58894b3dd3fcc7fbee5ea958bf20eff37d7dab86
deansher <deansherthompson@gmail.com>
2024-11-17 06:02:21 -0500

    improved editblock_prompts.py

--- aider/coders/editblock_prompts.py
+++ aider/coders/editblock_prompts.py
@@ -15,10 +15,12 @@ Use the appropriate shell based on the user's system info:
 {platform}
 Examples of when to suggest shell commands:
 
-- If you changed a self-contained html file, suggest an OS-appropriate command to open a browser to view it to see the updated content.
+- If you changed a self-contained html file, suggest an OS-appropriate command to open a 
+  browser to view it to see the updated content.
 - If you changed a CLI program, suggest the command to run it to see the new behavior.
 - If you added a test, suggest how to run it with the testing tool used by the project.
-- Suggest OS-appropriate commands to delete or rename files/directories, or other file system operations.
+- Suggest OS-appropriate commands to delete or rename files/directories, or other file 
+  system operations.
 - If your code changes add new dependencies, suggest the command to install them.
 - Etc.
 """
@@ -83,7 +85,8 @@ mathweb/flask/app.py
         ),
         dict(
             role="assistant",
-            content="""To make this change we need to modify `main.py` and make a new file `hello.py`:
+            content="""To make this change we need to modify `main.py` and make a new file 
+`hello.py`:
 
 1. Make a new hello.py file with hello() in it.
 2. Remove hello() from main.py and replace it with an import.
@@ -116,11 +119,12 @@ from hello import hello
         ),
     ]
 
-    system_reminder = """
-# *SEARCH/REPLACE block* Rules:
+    system_reminder = """# *SEARCH/REPLACE block* Rules:
 
 Every *SEARCH/REPLACE block* must use this format:
-1. The *FULL* file path alone on a line, verbatim. No bold asterisks, no quotes around it, no escaping of characters, etc.
+
+1. The *FULL* file path alone on a line, relative to the project root, verbatim, with no 
+   punctuation. No bold asterisks, no quotes around it, no escaping of characters, etc.
 2. The opening fence and code language, eg: {fence[0]}python
 3. The start of search block: <<<<<<< SEARCH
 4. A contiguous chunk of lines to search for in the existing source code
@@ -129,32 +133,48 @@ Every *SEARCH/REPLACE block* must use this format:
 7. The end of the replace block: >>>>>>> REPLACE
 8. The closing fence: {fence[1]}
 
-Use the *FULL* file path, as shown to you by your partner.
+Use the *FULL* file path, as shown to you in <context>...</context>
 
-Every *SEARCH* section must *EXACTLY MATCH* the existing file content, character for character, including all comments, docstrings, etc.
-If the file contains code or other data wrapped/escaped in json/xml/quotes or other containers, you need to propose edits to the literal contents of the file, including the container markup.
+Every *SEARCH* section must *EXACTLY MATCH* the existing file content, character for 
+character, including all comments, docstrings, etc.
+If the file contains code or other data wrapped/escaped in json/xml/quotes or other 
+containers, you need to propose edits to the literal contents of the file, including the 
+container markup.
 
 *SEARCH/REPLACE* blocks will *only* replace the first match occurrence.
 Including multiple unique *SEARCH/REPLACE* blocks if needed.
-Include enough lines in each SEARCH section to uniquely match each set of lines that need to change.
+Include enough lines in each SEARCH section to uniquely match each set of lines that need to 
+change.
 
 Keep *SEARCH/REPLACE* blocks concise.
-Break large *SEARCH/REPLACE* blocks into a series of smaller blocks that each change a small portion of the file.
-Include just the changing lines, and a few surrounding lines if needed for uniqueness.
-Do not include long runs of unchanging lines in *SEARCH/REPLACE* blocks.
 
-Only create *SEARCH/REPLACE* blocks for files that your partner has added to the chat!
+Break large *SEARCH/REPLACE* blocks into a series of smaller blocks that each change a small 
+portion of the file.
 
-To move code within a file, use 2 *SEARCH/REPLACE* blocks: 1 to delete it from its current location, 1 to insert it in the new location.
+It is helpful for both you and your partner when a *SEARCH/REPLACE* block can start at
+a logical point in the file, such as the beginning of a top-level declaration or the header
+of a document section. But try to keep the unchanged context at the top and bottom to
+between 10 and 20 lines. This provides enough context for the change without burying the
+change in a large block of unchanged code.
 
-Pay attention to which filenames your partner wants you to edit, especially if they are asking you to create a new file.
+Only write *SEARCH/REPLACE* blocks for files in <editable_files>...</editable_files> or
+files that you propose to create. If you feel strongly that you need to change a file in
+<readonly_files>...</readonly_files> that is within this project, you can write an edit
+block for that.
+
+To move code within a file, use 2 *SEARCH/REPLACE* blocks: 1 to delete it from its current 
+location, 1 to insert it in the new location.
+
+Pay attention to which filenames your partner wants you to edit, especially if they are 
+asking you to create a new file.
 
 If you want to put code in a new file, use a *SEARCH/REPLACE block* with:
-- A new file path, including dir name if needed
+- A new file path relative to the project root
 - An empty `SEARCH` section
 - The new file's contents in the `REPLACE` section
 
-To rename files which have been added to the chat, use shell commands at the end of your response.
+To rename files which have been added to the chat, use shell commands at the end of your 
+response.
 
 {lazy_prompt}
 ONLY EVER RETURN CODE IN A *SEARCH/REPLACE BLOCK*!
@@ -164,10 +184,12 @@ ONLY EVER RETURN CODE IN A *SEARCH/REPLACE BLOCK*!
     shell_cmd_reminder = """
 Examples of when to suggest shell commands:
 
-- If you changed a self-contained html file, suggest an OS-appropriate command to open a browser to view it to see the updated content.
+- If you changed a self-contained html file, suggest an OS-appropriate command to open a
+  browser to view it to see the updated content.
 - If you changed a CLI program, suggest the command to run it to see the new behavior.
 - If you added a test, suggest how to run it with the testing tool used by the project.
-- Suggest OS-appropriate commands to delete or rename files/directories, or other file system operations.
+- Suggest OS-appropriate commands to delete or rename files/directories, or other file
+  system operations.
 - If your code changes add new dependencies, suggest the command to install them.
 - Etc.
 """
@@ -183,11 +205,13 @@ Always reply to your partner in the same language they are using.
 
 Once you understand the request you MUST:
 
-1. Decide if you need to propose *SEARCH/REPLACE* edits to any files that haven't been added to the chat. You can create new files without asking!
+1. Decide if you need to propose *SEARCH/REPLACE* edits to any files that haven't been 
+   added to the chat. You can create new files without asking!
 
-But if you need to propose edits to existing files not already added to the chat, you *MUST* tell your partner their full path names and ask them to *add the files to the chat*.
-End your reply and wait for their approval.
-You can keep asking if you then decide you need to edit more files.
+   But if you need to propose edits to existing files not already added to the chat, you 
+   *MUST* tell your partner their full path names and ask them to *add the files to the chat*.
+   End your reply and wait for their approval.
+   You can keep asking if you then decide you need to edit more files.
 
 2. Think step-by-step and explain the needed changes in a few short sentences.
 
